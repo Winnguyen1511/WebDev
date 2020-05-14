4347df9562b26c09d790c332c61586ec0d9d5bb5
Winnguyen <khoanguyen1507vn@gmail.com>
2020-05-14 19:02:45 +0700

    update

--- BasicResponsive/Portfolio/script.js
+++ BasicResponsive/Portfolio/script.js
@@ -9,17 +9,29 @@ $(document).ready(function(){
         }, 350);
     });
 
-
-    $("#projects a").hover(function(){
+    $("#projects a").hover(
+        function(){
         //hander hover in
         // $(".code").fadeIn(300);
-        $(this).find(".code").fadeIn(300);
-        $(this).find(".code").css("color","var(--main-red)")
-    },
-    function(){
-        //hander hover out
-        // $(".code").fadeOut(300);
-        $(this).find(".code").fadeOut(300);
-        $(this).find(".code").css("color","var(--main-gray)")
-    });
+            $(this).find(".code").fadeIn(300);
+            $(this).find(".code").css("color","var(--main-red)")
+        },
+        function(){
+            //hander hover out
+            // $(".code").fadeOut(300);
+            $(this).find(".code").fadeOut(300);
+            // $(this).find(".code").css("color","var(--main-gray)");
+        }
+    );
+
+    $(".project-tile").hover(
+        function(){
+            //hover on
+            $(this).find("img").css("transform", "scale(1.2)");
+        },
+        function (){
+            //hover out
+            $(this).find("img").css("transform", "none");
+        }
+    );
 });
